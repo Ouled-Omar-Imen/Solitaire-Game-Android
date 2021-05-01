92d772a9f18cf58684971a72d40fc8a762215c78
ilyes <ouled.omar.ilyes@gmail.com>
2021-05-01 17:05:32 +0200

    updating solution

--- solitaire/app/src/main/java/com/example/solitaire/MainActivity.java
+++ solitaire/app/src/main/java/com/example/solitaire/MainActivity.java
@@ -16,33 +16,19 @@ import java.util.TimerTask;
 
 public class MainActivity extends Activity {
     private gameview gameview;
-    private Button btn_restart;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         gameview = findViewById(R.id.gameview);
-        btn_restart=findViewById(R.id.btn_restart);
-        btn_restart.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                finish();
-                startActivity(getIntent());
-                overridePendingTransition(0,0);
-            }
-
-        });
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //sauvgarder la partie en cours
-    }
-
+        @Override
+        protected void onPause () {
+            super.onPause();
+            //sauvgarder la partie en cours
+        }
 
 
     }
